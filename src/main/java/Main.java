7d2d34f6a4b0fd070a38cbecdfc50d7014b9359d
Christopher <christopher@Zipcoders-MacBook-Pro-26.local>
2023-03-01 18:59:26 -0500

    added set to track number of user guesses instead of incrementing an int with for loop and to make sure there are no duplicates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 
@@ -7,14 +8,16 @@ public class Main {
         int max = 50; //maximum number for range of generation
         int numGuesses = 0; //this will track the number of user's guesses
         int guess = 0; //this will track the value of the user's guesses
-        int answer = (int) Math.floor(Math.random() * (max - min + 1) + min); //randomly generate number between min and max
+        //int answer = (int) Math.floor(Math.random() * (max - min + 1) + min); //randomly generate number between min and max
+        int answer = 5; //controlled answer value for testing
+        HashSet<Integer> guessSet = new HashSet<Integer>(); //set for storing guesses
 
         while (guess != answer) { //loop will continue until user guesses the correct value
 
             Scanner input = new Scanner(System.in); //initializes scanner
             System.out.println("Guess the number"); //prompts user to enter their guess
             guess = input.nextInt();                //stores user input into int guess
-            numGuesses++;                           //increments the value of the numGuesses variable
+            guessSet.add(guess);                    //adds guess as entry into set
 
             if (guess < min || guess > max) { //checks if guess is below/above the expected range
                 System.out.println("You guess over or below the acceptable range of: " + min + " to " + max); //informs user they've guessed lower/higher than expected range
@@ -26,7 +29,7 @@ public class Main {
                 System.out.println("You guessed too low."); //informs user they've guessed too low
             }
             else {
-                System.out.println("Congrats. You guessed " + answer + " and it only took you " + numGuesses + " tries.");
+                System.out.println("Congrats. You guessed " + answer + " and it only took you " + guessSet.size() + " tries.");
             }
         }
     }
